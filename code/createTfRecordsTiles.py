876cd1150a580e9886a1b673d6c09ea3ba7884e3
Dmitry A. Grechka <dmitry@grechka.family>
2020-06-22 22:27:03 +0300

    tfRecords trainImagesTfRecords1024_224_neg_gcn_adaptQ_3_4_70_per_angle creation

--- code/createTfRecordsTiles.py
+++ code/createTfRecordsTiles.py
@@ -60,69 +60,73 @@ def ProcessGenerateRecordTask(task):
     #quantiles = [1/10, 1/8, 1/6, 1/5, 1/4, 1/3, 1/2, 2/3, 3/4, 4/5, 5/6, 7/8, 9/10, 1.0]
     quantiles = [3/4, 4/5, 5/6, 7/8, 9/10, 1.0]    
     activeQuantileIdx = 0
-    gatheredTiles = []
-    #print("Starting task {0} ({1},{2})".format(ident,h,w))
-    while len(gatheredTiles) < minRequiredTiles:
-        gatheredTiles = []
-        activeQuantile = quantiles[activeQuantileIdx]
-        for i in range(0,M):
-            effectiveDegree = rotStep*i
+    for i in range(0,M):
+        tiles = []
+        tfrecordsPathIdx = "{0}-{1}.tfrecords".format(tfrecordsPath[0:-10], i)
+        effectiveDegree = rotStep*i
+        
+        #print("Starting task {0} ({1},{2})".format(ident,h,w))
+        while len(tiles) < minRequiredTiles:
+            activeQuantile = quantiles[activeQuantileIdx]
             #print("rotating for {0}".format(effectiveDegree))
             if effectiveDegree != 0.0:
+                #print("rotating to {0}".format(effectiveDegree))
                 rotated = npImTrans.RotateWithoutCrop(im, effectiveDegree)
             else:
                 rotated = im
             #print("getting tiles")
             _,tiles = getNotEmptyTiles(rotated, tileSize, emptyCuttOffQuantile=activeQuantile)
 
-            if len(tiles) == 0:
-                #print("angle {0} for {1} results in 0 tiles. skipping this angle".format(effectiveDegree, ident))
-                sys.stdout.write("0")
-                sys.stdout.flush()
-                continue
+            #print("got {0} tiles ".format(len(tiles)))
+            # if len(tiles) == 0:
+            #     #print("angle {0} for {1} results in 0 tiles. skipping this angle".format(effectiveDegree, ident))
+            #     sys.stdout.write("0")
+            #     sys.stdout.flush()
+            #     continue
 
             #print("normalizing")
 
-            # normalizing with contrasts
-            contrasts = []
-            means = []
-            for tile in tiles:        
-                mu = npImNorm.getImageMean_withoutPureBlack(tile)
-                contrast = npImNorm.getImageContrast_withoutPureBlack(tile, precomputedMu=mu)
-                means.append(mu)
-                contrasts.append(contrast)
-            meanContrast = np.mean(contrasts)    
-            meanMean = np.mean(means)
-            for i in range(0,len(tiles)):
-                tiles[i] = npImNorm.GCNtoRGB_uint8(npImNorm.GCN(tiles[i], lambdaTerm=0.0, precomputedContrast=meanContrast, precomputedMean=meanMean), cutoffSigmasRange=1.0)
-            #print("resizing")
+            if len(tiles) < minRequiredTiles:
+                if activeQuantileIdx == (len(quantiles) - 1):
+                    if len(tiles) == 0:
+                        print("WARN: Image {0} resulted in 0 tiles. producing blank (black) single tile TfRecords file".format(ident))
+                        tiles = [ np.zeros((outImageSize,outImageSize,3),dtype=np.uint8) ]
+                    break
+                else:
+                    activeQuantileIdx += 1
+                    sys.stdout.write("[q{0:.2f}]".format(quantiles[activeQuantileIdx]))
+                    sys.stdout.flush()
+                    continue
+            
 
-            if outImageSize != tileSize:
-                resizedTiles = []
-                for tile in tiles:
-                    resizedTiles.append(cv2.resize(tile, dsize=(outImageSize, outImageSize), interpolation=cv2.INTER_AREA))
-                tiles = resizedTiles
-            gatheredTiles.append(tiles)
-            sys.stdout.write(".")
-            sys.stdout.flush()
+        # normalizing with contrasts
+        contrasts = []
+        means = []
+        #print("normalzing {0} tiles".format(len(tiles)))
+        for tile in tiles:        
+            mu = npImNorm.getImageMean_withoutPureBlack(tile)
+            contrast = npImNorm.getImageContrast_withoutPureBlack(tile, precomputedMu=mu)
+            means.append(mu)
+            contrasts.append(contrast)
+        meanContrast = np.mean(contrasts)    
+        meanMean = np.mean(means)
+        if meanMean > 0.0:
+            for j in range(0,len(tiles)):
+                tiles[j] = npImNorm.GCNtoRGB_uint8(npImNorm.GCN(tiles[j], lambdaTerm=0.0, precomputedContrast=meanContrast, precomputedMean=meanMean), cutoffSigmasRange=1.0)
+        #print("resizing")
 
-        gatheredTiles = [item for sublist in gatheredTiles for item in sublist]
-        if len(gatheredTiles) < minRequiredTiles:
-            if activeQuantileIdx == (len(quantiles) - 1):
-                if len(gatheredTiles) == 0:
-                    print("WARN: Image {0} resulted in 0 tiles. producing blank (black) single tile TfRecords file".format(ident))
-                    gatheredTiles = [ np.zeros((outImageSize,outImageSize,3),dtype=np.uint8) ]
-                break
-            else:
-                activeQuantileIdx += 1
-                sys.stdout.write("[q{0:.2f}]".format(quantiles[activeQuantileIdx]))
-                sys.stdout.flush()
-                
-        
-        
-    savePackAsTFRecord(gatheredTiles,tfrecordsPath)
-    sys.stdout.write("({0})".format(len(gatheredTiles)))
-    sys.stdout.flush()
+        if outImageSize != tileSize:
+            resizedTiles = []
+            for tile in tiles:
+                resizedTiles.append(cv2.resize(tile, dsize=(outImageSize, outImageSize), interpolation=cv2.INTER_AREA))
+            tiles = resizedTiles
+        sys.stdout.write(".")
+        sys.stdout.flush()
+
+        #print("saving {0}".format(len(tiles)))
+        savePackAsTFRecord(tiles,tfrecordsPathIdx)
+        sys.stdout.write("({0}:{1})".format(i,len(tiles)))
+        sys.stdout.flush()
     #print("done")
 
     # debug preview
@@ -151,7 +155,7 @@ def ProcessGenerateRecordTask(task):
         #         idx = idx + 1
         # plt.show()  # display it
 
-    return len(gatheredTiles)
+    return len(tiles)
 
 if __name__ == '__main__':
 
@@ -178,8 +182,8 @@ if __name__ == '__main__':
     
     print("Detected {0} CPU cores".format(M))
     #M = 1
-    #M //= 2 # opencv uses multithreading somehow. So we use less workers that CPU cores available
-    M = 2
+    M //= 2 # opencv uses multithreading somehow. So we use less workers that CPU cores available
+    #M = 2
 
     p = multiprocessing.Pool(M)
     print("Created process pool of {0} workers".format(M))
@@ -188,14 +192,20 @@ if __name__ == '__main__':
     tiffFiles = [x for x in files if x.endswith(".tiff")]
 
     # uncomment for short (test) run
-    # tiffFiles = tiffFiles[0:20]
+    #tiffFiles = tiffFiles[0:20]
 
     tasks = list()
     existsList = list()
 
     for tiffFile in tiffFiles:
         tfPath = os.path.join(outPath,"{0}.tfrecords".format(tiffFile[:-5]))
-        if(os.path.exists(tfPath)):
+
+        allFound = True
+        for i in range(0,rotationStepsCount):
+            if(not(os.path.exists("{0}-{1}.tfrecords".format(tfPath[0:-10],i)))):
+                allFound = False
+                break
+        if allFound:
             existsList.append(tfPath)
         else:
             task = {
@@ -221,23 +231,23 @@ if __name__ == '__main__':
 
     tilesCounts = p.map(ProcessGenerateRecordTask,tasks)
     #tilesCounts = [ProcessGenerateRecordTask(x) for x in tasks] # single threaded debugging
-    print("Analyzing tile count frequencies")
+    # print("Analyzing tile count frequencies")
 
-    tilesCounts = tilesCounts.extend(existingTilesCounts)
+    # tilesCounts = tilesCounts.extend(existingTilesCounts)
 
-    freqDict = dict()
-    for tilesCount in tilesCounts:                
-        if tilesCount in freqDict:
-            prevCount = freqDict[tilesCount]
-        else:
-            prevCount = 0
-        freqDict[tilesCount] = prevCount + 1
+    # freqDict = dict()
+    # for tilesCount in tilesCounts:                
+    #     if tilesCount in freqDict:
+    #         prevCount = freqDict[tilesCount]
+    #     else:
+    #         prevCount = 0
+    #     freqDict[tilesCount] = prevCount + 1
 
-    freqDf = pd.DataFrame.from_dict(freqDict, orient='index', columns=["Counts"])    
-    freqDf.sort_index(inplace=True)
-    freqDf.to_csv(outHistFile)
-    print(freqDf)
-    print("Tile count histogram written")
+    # freqDf = pd.DataFrame.from_dict(freqDict, orient='index', columns=["Counts"])    
+    # freqDf.sort_index(inplace=True)
+    # freqDf.to_csv(outHistFile)
+    # print(freqDf)
+    # print("Tile count histogram written")
 
     #with open(outIdxFile, 'w', encoding='utf-8') as f:
     #    json.dump(tileIdxDict, f, ensure_ascii=False)
